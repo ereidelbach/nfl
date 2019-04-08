2fb9e1ed752fa5da0b4992e0b6ea6e23cfdaf88b
eric.reidelbach <eric.reidelbach@gmail.com>
2019-04-08 15:16:13 -0400

    completed code for merging college and nfl data

--- Python Code/merge_nfl_to_ncaa.py
+++ Python Code/merge_nfl_to_ncaa.py
@@ -7,7 +7,7 @@ Created on Fri Apr  5 10:04:09 2019
 
 :DESCRIPTION:
     Merges NFL data scraped from pro-reference with college data scraped from
-        sports-reference.
+        sports-reference and ESPN (meta_DEF.csv and meta_OFF.csv).
 
 :REQUIRES:
     Refer to the Package Import section of the script
@@ -19,53 +19,334 @@ Created on Fri Apr  5 10:04:09 2019
 #==============================================================================
 # Package Import
 #==============================================================================
+import json
 import os  
+import numpy as np
 import pandas as pd
 import pathlib
 import tqdm
 
+from ast import literal_eval
+
 #==============================================================================
 # Reference Variable Declaration
 #==============================================================================
-
+dict_sort_variable = {'LB':'lastTackles',
+                      'DB':'lastPBU',
+                      'DL':'lastTFL',
+                      'Other':'lastTackles',
+                      'QB':'last',
+                      'RB':'last',
+                      'WR':'last',
+                      'TE':'last',
+                      }
+dict_positions = {'LB':['LB', 'OLB', 'ILB', 'MLB'],
+                  'DB':['CB', 'S', 'DB'],
+                  'DL':['DE', 'DT', 'DL', 'NT'],
+                  'QB':['QB'],
+                  'RB':['SB','HB','RB','FB'],
+                  'WR':['WR'],
+                  'TE':['TE'],
+        }
+list_variables_nfl = ['Year',
+                      'Age',
+                      'position_nfl',
+                      'Player',
+                      'nameFirst',
+                      'nameLast',
+                      'Team',
+                      'birthday',
+                      'hometownCity',
+                      'hometownState',
+                      'heightInches_nfl',
+                      'weight_nfl',
+                      'School',
+                      'ID_SportsRef_ncaa',
+                      'ID_SportsRef_nfl',
+                      'urlSportsRefNCAA',
+                      'urlSportsRefNFL',
+                      'picturePlayerURL',
+                      'pictureNflURL',
+                      'pictureSchoolURL'
+                      ]
+list_variables_merged = ['player',
+                         'school',
+                         'team_nfl',
+                         'pos_nfl',
+                         'pos_sr_0',
+                         'pos_sr_1',
+                         'pos_sr_2',
+                         'pos_espn',
+                         'name_first',
+                         'name_last',
+                         'name_espn',
+                         'age_last',
+                         'year_last',
+                         'height_nfl',
+                         'height_inches_nfl',
+                         'weight_nfl',
+                         'height_ncaa',
+                         'heightInches_ncaa',
+                         'weight_ncaa',
+                         'home',
+                         'home_city',
+                         'home_state',
+                         'birthday',
+                         'draft_overall',
+                         'draft_round',
+                         'draft_team',
+                         'draft_year',
+                         'id_sr_ncaa',
+                         'id_sr_nfl',
+                         'url_sr_ncaa',
+                         'url_sr_nfl',
+                         'url_espn_ncaa',
+                         'url_pic_player',
+                         'url_pic_team_ncaa',
+                         'url_pic_team_nfl'
+                         ]
+dict_var_rename = {'Year':'year_last',
+                   'Team':'team_nfl', 
+                   'Age':'age_last', 
+                   'height':'height_ncaa', 
+                   'heightInches':'heightInches_ncaa',
+                   'weight':'weight_ncaa',
+                   'ID_SportsRef_ncaa':'id_sr_ncaa',
+                   'ID_SportsRef_nfl':'id_sr_nfl',
+                   'urlSportsRefNCAA':'url_sr_ncaa',
+                   'urlSportsRefNFL':'url_sr_nfl',
+                   'pictureNflURL':'url_pic_team_nfl',
+                   'nameESPN':'name_espn',
+                   'position_0':'pos_sr_0',
+                   'position_1':'pos_sr_1',
+                   'position_2':'pos_sr_2',
+                   'position_nfl':'pos_nfl',
+                   'positionESPN':'pos_espn',
+                   'draftPick':'draft_overall',
+                   'draftRound':'draft_round',
+                   'draftTeam':'draft_team',
+                   'draftYear':'draft_year',
+                   'hometown':'home',
+                   'urlESPN':'url_espn_ncaa',
+                   'Player':'player',
+                   'School':'school',
+                   'nameFirst':'name_first',
+                   'nameLast':'name_last',
+                   'hometownCity':'home_city',
+                   'hometownState':'home_state',
+                   'picturePlayerURL':'url_pic_player',
+                   'pictureSchoolURL':'url_pic_team_ncaa',
+                   'heightNFL':'height_nfl',
+                   'heightInches_nfl':'height_inches_nfl'
+                   }
 #==============================================================================
 # Function Definitions
 #==============================================================================
-def function_name(var1, var2):
+def merge_nfl_ncaa_metadata(category):
     '''
-    Purpose: Stuff goes here
-
-    Inputs   
+    Purpose: Given scraped NFL metadata (from pro-football-reference.com) and
+        scraped NCAA metadata (from sports-reference.com), merge the two 
+        together to obtain a final, cohesive metadata record that spans both
+        leagues for all players.
+        
+    Inputs
     ------
-        var1 : type
-            description
-        var2 : type
-            description
-            
+        category : string
+            Type of data to merge (Two options: 'offense' or 'defense')
+    
     Outputs
     -------
-        var1 : type
-            description
-        var2 : type
-            description
+        df_merged : Pandas DataFrame
+            Contains merged metadata for all players for both NCAA and NFL data        
     '''
+    # error catcher -- only 'offense' and 'defense' are valid categories
+    if category not in ['offense', 'defense']:
+        print('Wrong category selected.  Please enter "offense" or "defense."')
+        return
+    
+    # ingest the nfl data
+    if category == 'offense':
+        df_nfl = pd.DataFrame()
+        for sub_cat in ['passing', 'receiving', 'rushing']:
+            df_temp = pd.read_csv('positionData/scraped_NFL/%s-2005-2018.csv' % sub_cat)
+            if len(df_nfl) == 0:
+                df_nfl = df_temp.copy()
+            else:
+                df_nfl = pd.concat([df_nfl, df_temp], sort = False, 
+                                           ignore_index = True)
+    elif category == 'defense':
+        df_nfl = pd.read_csv('positionData/scraped_NFL/%s-2005-2018.csv' % category)
+    
+    # remove duplicates that may exist across position groups
+    df_nfl.drop_duplicates(inplace = True)
+    
+    # only keep the last year for all players
+    df_nfl.drop_duplicates(inplace = True, subset = ['ID_SportsRef_nfl'], 
+                                   keep = 'last')
+    
+    # only keep desired variables
+    df_nfl = df_nfl[list_variables_nfl]
+    
+    # ingest the college data
+    if category == 'offense':
+        df_ncaa = pd.read_csv('positionData/Metadata/meta_OFF.csv')
+    elif category == 'defense':
+        df_ncaa = pd.read_csv('positionData/Metadata/meta_DEF.csv')
+            
+    # merge the nfl and college data
+    df_merged = pd.merge(df_nfl, df_ncaa, how = 'outer',
+                                 left_on = 'ID_SportsRef_ncaa', 
+                                 right_on = 'unique_id')
+      
+    # fill in variables to account for overlap across the two files
+    for variable in ['Player', 'School', 'nameFirst', 'nameLast', 'hometownCity',
+                     'hometownState', 'picturePlayerURL', 'pictureSchoolURL',
+                     'birthday']:
+        # keep the _x version of the variable unless it is missing, then go with _y
+        df_merged[variable] = df_merged.apply(lambda row:
+            row['%s_x' % variable] if pd.isna(row['%s_y' % variable]) 
+            else row['%s_y' % variable], axis = 1) 
+        # drop _x and _y variables once merged variable is created
+        df_merged = df_merged.drop(
+                ['%s_x' % variable, '%s_y' % variable], axis = 1)
+        
+    # create a new variable for NFL height (format: Ft - Inches)
+    df_merged['heightNFL'] = df_merged['heightInches_nfl'].apply(
+            lambda x: '-'.join([
+                    str(int(np.floor(x/12))), 
+                    str(int(x - np.floor(x/12)*12))]) if not pd.isna(x) else '')
+    
+    # correct issues where countries are stored in the city field
+    list_states = df_merged.apply(lambda row:
+        row['hometownCity'] if pd.isna(row['hometownState']) 
+        else row['hometownState'], axis = 1)
+    df_merged['hometownCity'] = df_merged.apply(lambda row:
+        '' if pd.isna(row['hometownState']) else row['hometownCity'], axis = 1)
+    df_merged['hometownState'] = list_states.copy()
+        
+    # fill in missing values with empty strings for hometownState and hometownCity
+    df_merged[['hometownCity', 'hometownState']] = df_merged[[
+            'hometownCity', 'hometownState']].fillna('')
+        
+    # fill in missing hometown values by recreating the variable
+    df_merged['hometown'] = df_merged.apply(lambda row:
+        ', '.join([row['hometownCity'], row['hometownState']]) 
+        if row['hometownCity'] != '' else row['hometownState'], axis = 1)
+        
+    # rename variables as desired
+    df_merged = df_merged.rename(dict_var_rename, axis = 1)
+        
+    # drop the variable 'unique_id' as it is no longer used
+    df_merged = df_merged.drop('unique_id', axis = 1)
+    
+    # put dataframe columns in desired order
+    df_merged = df_merged[list_variables_merged]
+    
+    # sort the dataframe by last name (i.e. 'name_last')
+    df_merged = df_merged.sort_values(by = 'name_last')
+    
+    # export the dataframe to a csv file
+    df_merged.to_csv('positionData/MetaData/merged_%s.csv' % category, 
+                     index = False)
+    
+    return df_merged
+
+def subsetDataByPosition(df_pos, position):
+    '''
+    Purpose: Given a DataFrame of players for a specific category of football
+        data (i.e. Passing, Rushing, Receiving or Defense), subset that data
+        such that individual files are created for specific positions.
+        
+        For example, given Passing data, subset the data such that only players
+        who played quarterback are in the data and save it as 'pos_QB.csv'
+
+    Inputs
+    ------
+        df_pos : Pandas Dataframe
+            DataFrame containing player information for a specific stat group
+                (i.e. Passing, Rushing, Receiving or Defensive)
+        position : string
+            Position group for which data exists and must be merged
+            (options are 'DEF', 'QB', RB', and 'WR')
+    
+    Outputs
+    -------
+        The subset dataframe is written to a .json and .csv file in /public
+    '''    
+
+    # reduce the dataset to contain only players of specific positions'
+    list_cols_pos = [x for x in df_pos.columns.tolist() if 'position' in x]
+    df_subset = pd.DataFrame()
+    # iterate over every position column and add players if they played that pos
+    for col in list_cols_pos:
+        players = df_pos[df_pos[col].isin(dict_positions[position])]
+        if len(df_subset) == 0:
+            df_subset = players.copy()
+        else:
+            df_subset = pd.concat([df_subset, players] , sort = False)
+            
+    # remove any duplicate rows created in this process
+    df_subset = df_subset.drop_duplicates()
+    
+    # sort the reduced dataset by the desired variable for the position group
+    df_subset = df_subset.sort_values(by = [dict_sort_variable[position]], 
+                                      ascending = False)
+    
+    # reset index
+    df_subset = df_subset.reset_index(drop = True)
+    
+    # create the `Index` variable
+    df_subset['Index'] = df_subset.index
+        
+    #------ Make sure columns with ELO data are stored as lists  of ints -----#
+    # obtain a list of all elo variables
+    list_elo = [x for x in list(df_subset.columns) if 'elo' in x.lower()]
+    # iterate through each elo variable and eval it so that it's a list
+    for elo in list_elo:
+        df_subset[elo] = df_subset[elo].apply(lambda x: literal_eval(x))
+        
+    #-------- Make sure opponent ids are stored as list of ints --------------#
+    list_temp = []
+    for row in df_subset['opp']:
+        try:
+            list_temp.append([int(x) for x in row.split(',')])
+        except:
+            list_temp.append('')
+            print(row)
+    df_subset['opp'] = list_temp
+    
+    #-------- Make sure the date column is stored as a list of string --------#
+    list_temp = []
+    for row in df_subset['date']:
+        list_temp.append([str(x) for x in row.split(',')])
+    df_subset['date'] = list_temp
+    #df_subset['date'] = df_subset['date'].apply(lambda x: literal_eval(x))
+    
+    # fill in missing values with blanks rather than float NaNs
+    df_subset = df_subset.fillna('')
+    
+    # make a new position variable and drop others
+    df_subset['position'] = position
+    df_subset = df_subset.drop(list_cols_pos[:-1], axis = 1)
+    list_cols_new = df_subset.columns.tolist()
+    list_cols_new.insert(6, list_cols_new.pop(len(list_cols_new)-1))
+    df_subset = df_subset[list_cols_new]
+    
+    return df_subset
+
 #==============================================================================
 # Working Code
 #==============================================================================
 
 # Set the project working directory
-path_dir = pathlib.Path('/home/ejreidelbach/Projects/')
+path_dir = pathlib.Path('/home/ejreidelbach/Projects/draft-gem/src/static/')
 os.chdir(path_dir)
 
-   
 #------------------------------------------------------------------------------
-# fill in missing values with blanks rather than float NaNs
-df_final = df_final.fillna('')
-    
-# Convert the dataframe to a dictionary
-dict_final = df_final.to_dict('records')    
-
-# Write updated data (with paths to pictures vice URLs) to a .json file 
-#   ('pos_POSITION_final_pics.json')
-with open('Data/SportsReference/qb_lookup.json', 'wt') as out:
-    json.dump(dict_final, out, sort_keys=True) 
+# Step 1. Combine NFL data with it's collegiate counterpart
+#------------------------------------------------------------------------------
+# OFFENSE 
+df_off = merge_nfl_ncaa_metadata('offense')
+ 
+# DEFENSE      
+df_def = merge_nfl_ncaa_metadata('defense')
---
+++ Python Code/scrape_nfl_combine.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 14:38:34 2019
+
+@author: ejreidelbach
+
+:DESCRIPTION: Scrapes combine data from 2000 to 2018 from ProFootball Reference
+    'https://www.pro-football-reference.com/draft/2019-combine.htm'
+
+:REQUIRES:
+   
+:TODO:
+"""
+ 
+#==============================================================================
+# Package Import
+#==============================================================================
+import datetime
+import os  
+import pandas as pd
+import pathlib
+import requests
+import tqdm
+import time
+
+from bs4 import BeautifulSoup
+from requests.packages.urllib3.util.retry import Retry
+from string import digits
+
+#==============================================================================
+# Reference Variable Declaration
+#==============================================================================
+
+#==============================================================================
+# Function Definitions
+#==============================================================================
+def soupifyURL(url):
+    '''
+    Purpose: Turns a specified URL into BeautifulSoup formatted HTML 
+
+    Inputs
+    ------
+        url : string
+            Link to the designated website to be scraped
+    
+    Outputs
+    -------
+        soup : html
+            BeautifulSoup formatted HTML data stored as a complex tree of 
+            Python objects
+    '''
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = requests.adapters.HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    
+    r = session.get(url)
+    #r = requests.get(url)
+    soup = BeautifulSoup(r.content,'html.parser')   
+    return soup
+
+def renameSchool(df, name_var):
+    '''
+    Purpose: Rename a school/university to a standard name as specified in 
+        the file `school_abbreviations.csv`
+
+    Inputs
+    ------
+        df : Pandas Dataframe
+            DataFrame containing a school-name variable for which the names
+            need to be standardized
+        name_var : string
+            Name of the variable which is to be renamed/standardized
+    
+    Outputs
+    -------
+        list(row)[0] : string
+            Standardized version of the school's name based on the first value
+            in the row in the file `school_abbreviations.csv`
+    '''  
+    # read in school name information
+    df_school_names = pd.read_csv('/home/ejreidelbach/Projects/draft-gem/' +
+                                  'src/static/positionData/' +
+                                  'pics.csv')
+     
+    # convert the dataframe to a dictionary such that the keys are the
+    #   optional spelling of each school and the value is the standardized
+    #   name of the school
+    dict_school_names = {}
+    
+    for index, row in df_school_names.iterrows():
+        # isolate the alternative name columns
+        names = row[[x for x in row.index if 'Name' in x]]
+        # convert the row to a list that doesn't include NaN values
+        list_names = [x for x in names.values.tolist() if str(x) != 'nan']
+        # add the nickname to the team names as an alternative name
+        nickname = row['Nickname']
+        list_names_nicknames = list_names.copy()
+        for name in list_names:
+            list_names_nicknames.append(name + ' ' + nickname)
+        # extract the standardized team name
+        name_standardized = row['Team']
+        # add the standardized name
+        list_names_nicknames.append(name_standardized)
+        # add the nickname to the standardized name
+        list_names_nicknames.append(name_standardized + ' ' + nickname)
+        # for every alternative spelling of the team, set the value to be
+        #   the standardized name
+        for name_alternate in list_names_nicknames:
+            dict_school_names[name_alternate] = name_standardized
+            
+    df[name_var] = df[name_var].apply(
+            lambda x: dict_school_names[x] if str(x) != 'nan' else '')
+        
+    return df    
+
+def scrapeCombineAllYears():
+    '''
+    Purpose: Scrape combine information for all players for all years
+        that is provided by pro-football-reference.com (sports-reference)
+        [https://www.pro-football-reference.com/draft/2019-combine.htm]
+        
+    Inputs
+    ------
+        NONE
+    
+    Outputs
+    -------
+        df_master : Pandas DataFrame
+            contains combine information for all players from all scraped years
+    '''  
+    # create a list for storing data from all years
+    list_dfs = []
+    
+    # process all available years
+    year_end = datetime.datetime.now().year + 1
+    year_start = 2000
+    
+    # Scrape data for all available years
+    for year in tqdm.tqdm(list(range(year_start, year_end))):
+        
+        # scrape page for combine year
+        url = ('https://www.pro-football-reference.com/draft/' 
+               + str(year) + '-combine.htm')
+        soup = soupifyURL(url)
+        
+        # Test for a year with no data (happens in new year w/o combine)
+        if 'Page Not Found' in str(soup):
+            continue
+        
+        # Retrieve the HTML of the combine table
+        table = soup.find('table', {'id':'combine'})
+        
+        # Convert the table to a dataframe
+        df_year = pd.read_html(str(table))[0]
+        df_year['Year'] = year
+        
+        # Drop Rows that contain header information (i.e. 'Player' == 'Player')
+        df_year = df_year[df_year['Player'] != 'Player']
+        
+        # Retrieve player's ncaa ID and ncaa profile URL
+        list_player_urls = []
+        list_player_ids = []
+        for row in table.find_all('tr'):
+            # the first column of each row has what we need
+            col = row.find('th')
+            if col.text != 'Player':
+                if col.find('a') is not None:
+                    list_player_urls.append(col.a['href'])
+                    list_player_ids.append(col['data-append-csv'])
+                else:
+                    list_player_urls.append('')
+                    list_player_ids.append('')
+                    
+        # Create the variables: 'id_sr_ncaa' and 'url_sr_ncaa'
+        df_year['url_sr_ncaa'] = list_player_urls
+        df_year['id_sr_ncaa'] = list_player_ids
+        
+        # Add the data for the year to the list
+        list_dfs.append(df_year)
+        
+        # Wait a second to slow down the rate of scraping
+        time.sleep(1)
+        
+    # convert the data from all years into one file
+    df_master = pd.DataFrame()
+    for df in list_dfs:
+        if len(df_master) == 0:
+            df_master = df.copy()
+        else:
+            df_master = df_master.append(df)
+  
+    # Standardize Variables and Formatting of dataframe
+    df_final = fixCombineInfo(df_master)
+    
+    # Write the file to csv
+    df_final.to_csv('positionData/Combine/combine_%s_to_%s.csv' % 
+                    (year_start, year_end))
+        
+    return df_final
+
+def scrapeCombineSpecificYear(year):
+    '''
+    Purpose: Scrape combine information for all players for the specified year.
+        Data is scraped from pro-football-reference.com (sports-reference)
+        [https://www.pro-football-reference.com/draft/2019-combine.htm]
+        
+    Inputs
+    ------
+        year : int
+            desired year of combine info to scrape 
+    
+    Outputs
+    -------
+        df_master : Pandas DataFrame
+            contains combine information for all players from all scraped years
+    '''  
+    # scrape data for requested year
+    url = ('https://www.pro-football-reference.com/draft/' 
+           + str(year) + '-combine.htm')
+    soup = soupifyURL(url)
+
+    # Retrieve the HTML of the combine table    
+    table = soup.find('table', {'id':'combine'})
+
+    # Convert the table to a dataframe    
+    df_year = pd.read_html(str(table))[0]
+    df_year['Year'] = year  
+    
+    # Drop Rows that contain header information (i.e. 'Player' == 'Player')
+    df_year = df_year[df_year['Player'] != 'Player']
+    
+    # Retrieve player's ncaa ID and ncaa profile URL
+    list_player_urls = []
+    list_player_ids = []
+    for row in table.find_all('tr'):
+        # the first column of each row has what we need
+        col = row.find('th')
+        if col.text != 'Player':
+            if col.find('a') is not None:
+                list_player_urls.append(col.a['href'])
+                list_player_ids.append(col['data-append-csv'])
+            else:
+                list_player_urls.append('')
+                list_player_ids.append('')
+                
+    # Create the variables: 'id_sr_ncaa' and 'url_sr_ncaa'
+    df_year['url_sr_ncaa'] = list_player_urls
+    df_year['id_sr_ncaa'] = list_player_ids
+    
+    # Standardize Variables and Formatting of dataframe
+    df_final = fixCombineInfo(df_year)
+    
+    return df_final
+        
+def fixCombineInfo(df_input):
+    '''
+    Purpose: Standardize combine information (including fixing variables
+        and creating new ones)
+        
+    Inputs
+    ------
+        df_input : Pandas DataFrame
+            contains combine information
+    
+    Outputs
+    -------
+        df : Pandas DataFrame
+            contains combine information with standardized/corrected information
+    '''
+    df = df_input.copy()
+
+    # remove the stats variable `College` which contained a link that didn't soupify
+    df.drop(columns = ('College'), inplace = True)
+
+    # set data types of numeric variables to a float
+    df[['Wt', '40yd', 'Vertical', 'Bench', 'Broad Jump', '3Cone', 'Shuttle', 
+        'Year']] = df[['Wt', '40yd', 'Vertical', 'Bench', 'Broad Jump', 
+               '3Cone', 'Shuttle', 'Year']].apply(lambda x: x.astype(float))
+    
+    # split the `Drafted (tm/rnd/yr)` variable into 3 variables (1 for each category)
+    df['DraftTeam'] = df['Drafted (tm/rnd/yr)'].apply(
+            lambda x: x.split('/')[0].strip() if not pd.isna(x) else '')
+    df['DraftRound'] = df['Drafted (tm/rnd/yr)'].apply(
+            lambda x: x.split('/')[1].strip()[0] if not pd.isna(x) else '')
+    df['DraftPick'] = df['Drafted (tm/rnd/yr)'].apply(
+            lambda x: x.split('/')[2].strip() if not pd.isna(x) else '')
+    df['DraftPick'] = df['DraftPick'].apply(lambda pick:
+            ''.join(x for x in pick if x in digits))
+    df['DraftYear'] = df['Drafted (tm/rnd/yr)'].apply(
+            lambda x: x.split('/')[-1].strip() if not pd.isna(x) else '')
+        
+    # remove the old `Drafted (tm/rnd/yr)` variable
+    df.drop(columns = ('Drafted (tm/rnd/yr)'), inplace = True)
+    
+    # add a `HeightInches` variable
+    df['HtInches'] = df['Ht'].apply(lambda height:
+            (int(height.split("-")[0])*12 + int(height.split("-")[1])))
+        
+    # add the nameFirst and nameLast variables
+    df['nameFirst'] = df['Player'].apply(lambda x: x.split(' ')[0])
+    df['nameLast'] = df['Player'].apply(lambda x: ' '.join(x.split(' ')[1:]))
+        
+    # reorder the columns
+    df = df[['Year', 'Player', 'nameFirst', 'nameLast', 'Pos', 'School', 'Ht', 
+             'HtInches', 'Wt', '40yd', 'Vertical', 'Bench', 'Broad Jump',
+             '3Cone', 'Shuttle', 'DraftTeam', 'DraftRound',
+             'DraftPick', 'DraftYear', 'id_sr_ncaa', 'url_sr_ncaa']]
+        
+    return df
+
+#=============================================================================
+# Working Code
+#==============================================================================
+
+# Set the project working directory
+path_dir = pathlib.Path('/home/ejreidelbach/Projects/draft-gem/src/static')
+os.chdir(path_dir)
+
+# Scrape Data for all years
+df = scrapeCombineAllYears()
+
+# Scrape Data for specific years
+#df = scrapeCombineSpecificYear(2019)    
